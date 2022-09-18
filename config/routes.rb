ff49612bebb95eaa02e9646ae47ee411678f9c57
Abinash <abi.kh03@gmail.com>
2022-09-18 22:54:02 +1000

    rough checking bootstrap addition to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # get 'start/index'
+  # get 'start/new'
+  # get 'start/create'
+  # get 'start/update'
+  # get 'start/edit'
+  # get 'start/destroy'
+  # get 'start/show'
+resources :start
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
